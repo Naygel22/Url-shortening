c28ab13cb4fa9287d391bf90141699f994905dc2
Naygel22 <piotrkozlowski14@gmail.com>
2024-03-13 20:40:18 +0100

    UI adjustments

--- Url-shortening/src/components/Footer.tsx
+++ Url-shortening/src/components/Footer.tsx
@@ -3,29 +3,29 @@ export function Footer() {
     <div className="footer">
       <div className="footerLogo">Shortly</div>
 
-      <div className="footerLinks column2">
-        <div>Features</div>
+      <div className="footerLinks column3">
+        <div className="footerLinksTitle">Features</div>
         <div>Link shortening</div>
         <div>Branded Links</div>
         <div>Analytics</div>
       </div>
 
-      <div className="footerLinks column2">
-        <div>Resources</div>
+      <div className="footerLinks column4">
+        <div className="footerLinksTitle">Resources</div>
         <div>Blog</div>
         <div>Developers</div>
         <div>Support</div>
       </div>
 
-      <div className="footerLinks column3">
-        <div>Company</div>
+      <div className="footerLinks column5">
+        <div className="footerLinksTitle">Company</div>
         <div>About</div>
         <div>Our Team</div>
         <div>Careers</div>
         <div>Contact</div>
       </div>
 
-      <div className="footerIcons column3">
+      <div className="footerIcons column6">
         <img src="public/images/icon-facebook.svg" alt="facebook icon" />
         <img src="public/images/icon-twitter.svg" alt="twitter icon" />
         <img src="public/images/icon-pinterest.svg" alt="pinterest icon" />
